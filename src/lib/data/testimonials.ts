0c27bd88beaf0a494141136fd385649ec7f7048a
--Ax- <andreiamzu@yahoo.com>
2025-03-31 22:47:28 +0200

    🩹 Replace mock testimonials

--- src/lib/data/testimonials.ts
+++ src/lib/data/testimonials.ts
@@ -3,46 +3,11 @@ import type { Testimonial } from '$lib/types/testimonials';
 export const testimonials: Testimonial[] = [
 	{
 		avatar: '/testimonials/placeholder.png',
-		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		text: 'Soon',
 		name: 'John Doe',
 		company: 'Company A',
-		linkedin: 'https://www.linkedin.com/in/andrei-alexandru-amzu-5123132a0/',
-		website: 'https://www.linkedin.com/in/andrei-alexandru-amzu-5123132a0/',
+		linkedin: 'https://www.linkedin.com',
+		website: 'https://www.linkedin.com',
 		position: 'CEO'
-	},
-	{
-		avatar: '/testimonials/placeholder.png',
-		text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		name: 'Jane Smith',
-		company: 'Company B',
-		position: 'CTO'
-	},
-	{
-		avatar: '/testimonials/placeholder.png',
-		text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-		name: 'Alice Johnson',
-		company: 'Company C',
-		position: 'CFO'
-	},
-	{
-		avatar: '/testimonials/placeholder.png',
-		text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-		name: 'Alice Johnson',
-		company: 'Company C',
-		position: 'CFO'
-	},
-	{
-		avatar: '/testimonials/placeholder.png',
-		text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-		name: 'Alice Johnson',
-		company: 'Company C',
-		position: 'CFO'
-	},
-	{
-		avatar: '/testimonials/placeholder.png',
-		text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-		name: 'Alice Johnson',
-		company: 'Company C',
-		position: 'CFO'
 	}
 ];
